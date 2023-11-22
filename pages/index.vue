3999a5107292b5757de228eb09128086355a6f95
plombir1917 <dfhhggh@list.ru>
2023-11-22 22:06:36 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>{{ newSession }}</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data: () => ({
+    newSession: [],
+  }),
+
+  async mounted() {
+    this.newSession = await this.$axios.$post(
+      'https://lstu.fusionsoft.ru/srv-elevator/newSession?timeFactor=1.0&floors=3&clientName=захар'
+    )
+  },
+})
+</script>
