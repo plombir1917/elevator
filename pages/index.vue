04ef7353b9a69624afb448af59a7f14ee140abd1
plombir1917 <dfhhggh@list.ru>
2023-11-23 13:35:48 +0300

    feat: elevator template

--- pages/index.vue
+++ pages/index.vue
@@ -13,97 +13,123 @@
         </b-collapse>
       </b-navbar>
     </div>
-    <div class="form">
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group
-          id="input-group-3"
-          label="Быстро вверх:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.MoveUpFast"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+    <main>
+      <div class="house">
+        <table width="20%" cellspacing="0" cellpadding="63">
+          <tr id="3">
+            <td width="200" valign="top"></td>
+            <td id="third_floor" valign="top">
+              <button id="third_floor" inner-height="70"></button>
+            </td>
+          </tr>
+          <tr id="2">
+            <td width="200" valign="top"></td>
+            <td id="second_floor" valign="top">
+              <button id="second_floor" inner-height="70"></button>
+            </td>
+          </tr>
+          <tr id="1">
+            <td class="lift" width="5" valign="top"></td>
+            <td id="first_floor" valign="top">
+              <button id="first_floor" inner-height="70"></button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="options">
+        <div class="form">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+              id="input-group-3"
+              label="Быстро вверх:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.MoveUpFast"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Медленно вверх:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.MoveUpSlow"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Медленно вверх:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.MoveUpSlow"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Быстро вниз:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.MoveDownFast"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Быстро вниз:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.MoveDownFast"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Медленно вниз:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.MoveDownSlow"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Медленно вниз:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.MoveDownSlow"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Закрыть дверь:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.DoClose"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Закрыть дверь:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.DoClose"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-form-group
-          id="input-group-3"
-          label="Открыть дверь:"
-          label-for="input-3"
-        >
-          <b-form-select
-            id="input-3"
-            v-model="nextCycle.DoOpen"
-            :options="options"
-            required
-          ></b-form-select>
-        </b-form-group>
+            <b-form-group
+              id="input-group-3"
+              label="Открыть дверь:"
+              label-for="input-3"
+            >
+              <b-form-select
+                id="input-3"
+                v-model="nextCycle.DoOpen"
+                :options="options"
+                required
+              ></b-form-select>
+            </b-form-group>
 
-        <b-button type="submit" variant="primary">Отправить</b-button>
-        <b-button type="reset" variant="danger">Сбросить</b-button>
-      </b-form>
-    </div>
-    <b-button
-      variant="outline-primary"
-      v-if="newSession.sessionId"
-      class="newCycle_button"
-      @click.prevent="startNextCycle(newSession.sessionId)"
-      >Новый цикл</b-button
-    >
+            <b-button type="submit" variant="primary">Отправить</b-button>
+            <b-button type="reset" variant="danger">Сбросить</b-button>
+          </b-form>
+        </div>
+        <b-button
+          variant="outline-primary"
+          v-if="newSession.sessionId"
+          class="newCycle_button"
+          @click.prevent="startNextCycle(newSession.sessionId)"
+          >Новый цикл</b-button
+        >
+      </div>
+    </main>
     <p v-if="cycleRes.sensors !== null">{{ cycleRes }}</p>
   </section>
 </template>
@@ -174,12 +200,26 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+.first_floor {
+  font-size: 10;
+}
+.lift {
+  background: whitesmoke;
+}
+.house {
+}
 section {
   text-align: center;
 }
 
+main {
+  display: flex;
+  justify-content: center;
+}
+
+.options {
+}
+
 .form {
-  margin: 0 auto;
-  max-width: 20%;
 }
 </style>
